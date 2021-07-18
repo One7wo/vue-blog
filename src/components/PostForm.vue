ca9fb6365e4ca49bb901bdd67ff6d8376d80e527
OneTwo <gonchar.dev@yandex.ru>
2021-07-18 19:12:14 +0300

    start app

---
+++ src/components/PostForm.vue
@@ -0,0 +1,81 @@
+<template>
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <input
+      v-model="post.title"
+      class="input"
+      type="text"
+      placeholder="Название"
+    />
+    <input
+      v-model="post.body"
+      class="input"
+      type="text"
+      placeholder="Описание"
+    />
+    <button class="create" @click="createPost">Создать</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: "",
+        body: "",
+      },
+    };
+  },
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      };
+      this.posts.push(newPost);
+      this.title = "";
+      this.body = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+}
+form h4 {
+  margin-bottom: 10px;
+}
+.input {
+  padding: 15px 10px;
+  border-radius: 8px;
+  background: #fff;
+  outline: none;
+  border: none;
+  margin-bottom: 10px;
+}
+.create {
+  background: #fff;
+  color: #6ea36e;
+  font-weight: bold;
+  padding: 10px;
+  width: 150px;
+  outline: none;
+  border: 1px solid #6ea36e;
+  border-radius: 8px;
+  cursor: pointer;
+  /* margin: 0 auto; */
+  align-self: flex-end;
+}
+.create:hover {
+  background: #6ea36e;
+  font-weight: bold;
+  color: #fff;
+  border: 1px solid #6ea36e;
+}
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="post" v-for="post in posts">
+    <div><strong>Название:</strong> {{ post.title }}</div>
+    <div><strong>Описание:</strong> {{ post.body }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    posts: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.post {
+  padding: 15px;
+  background: #ffffff;
+  border-radius: 8px;
+  margin-bottom: 15px;
+}
+.post:last-child {
+  margin-bottom: 0px;
+}
+</style>
